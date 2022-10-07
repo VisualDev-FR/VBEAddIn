ae7136fb757d22b043bb85f46f7d359f35e5f51e
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-10-07 20:02:18 +0200

    Initial Commit

---
+++ Connect.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.Office.Interop
+Imports Extensibility
+Imports System.Windows.Forms
+Imports System.Runtime.InteropServices
+Imports Microsoft.Vbe.Interop
+Imports Microsoft.Office.Core
+
+<ComVisible(True), Guid("875B3991-9A51-48AC-A328-ABE02EB53279"), ProgId("VBEAddIn.Connect")>
+Public Class Connect
+    Implements Extensibility.IDTExtensibility2
+
+    Private _VBE As VBE
+    Private _AddIn As AddIn
+
+    Private Sub OnConnection(Application As Object, ConnectMode As Extensibility.ext_ConnectMode,
+       AddInInst As Object, ByRef custom As System.Array) Implements IDTExtensibility2.OnConnection
+        Try
+            _VBE = DirectCast(Application, VBE)
+            _AddIn = DirectCast(AddInInst, AddIn)
+            Select Case ConnectMode
+                Case Extensibility.ext_ConnectMode.ext_cm_Startup
+                Case Extensibility.ext_ConnectMode.ext_cm_AfterStartup
+                    InitializeAddIn()
+            End Select
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub OnDisconnection(RemoveMode As Extensibility.ext_DisconnectMode,
+       ByRef custom As System.Array) Implements IDTExtensibility2.OnDisconnection
+
+    End Sub
+
+    Private Sub OnStartupComplete(ByRef custom As System.Array) _
+       Implements IDTExtensibility2.OnStartupComplete
+        InitializeAddIn()
+    End Sub
+
+    Private Sub OnAddInsUpdate(ByRef custom As System.Array) Implements IDTExtensibility2.OnAddInsUpdate
+
+    End Sub
+
+    Private Sub OnBeginShutdown(ByRef custom As System.Array) Implements IDTExtensibility2.OnBeginShutdown
+
+    End Sub
+
+    Private Sub InitializeAddIn()
+        'MessageBox.Show(_AddIn.ProgId & " loaded in VBA editor version " & _VBE.Version)
+
+        Dim vbProj As Object
+        For Each vbProj In _VBE.VBProjects
+
+            Dim m_Window As Microsoft.Vbe.Interop.Window = _VBE.Windows.CreateToolWindow(_AddIn, "VBEAddIn.Connect", "MyWindow", "AnyString", Nothing)
+
+        Next
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBEAddin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VBEAddin")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ead31314-ea00-455a-8ff8-750ec04692e2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
